6f1981a37ffd3526ff62291d5840a7cdca1fcd41
Fajrul Falah <fajrulf960@gmail.com>
2023-04-08 23:20:04 +0700

    landingpage v1.0

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,305 @@
 <x-main-layout>
-    <div class="flex justify-center flex-col items-center gap-4">
-        <div class="text-2xl">
-            Selamat datang di website JadiUMKM
-        </div>
 
-        @if (Auth::user())
+    <div class="menu-overlay"></div>
+
+    <!-- Slider Section Start -->
+    <div class="section slider-section">
+
+        <div class="slider-shape"></div>
+
+        <div class="container">
+            <div class="slider-active">
+                <div class="swiper-container">
+                    <div class="swiper-wrapper">
+
+                        <!-- Single Slider Start  -->
+                        <div class="single-slider swiper-slide animation-style-01">
+
+                            <!-- Slider Content Start -->
+                            <div class="slider-content">
+                                <h2 class="title">Tingkatkan Branding <br> Usahamu Sekarang</h2>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas tempora repellat, labore sint itaque nihil dolor eaque accusantium, accusamus alias voluptatum tempore laborum.</p>
+                                <a href="product-details.html" class="btn btn-primary btn-hover-dark">Join Us</a>
+                            </div>
+                            <!-- Slider Content End -->
+
+                            <!-- Slider images Start -->
+                            <div class="slider-images">
+                                <img src="assets/images/slider/slider1.png" alt="Slider">
+                            </div>
+                            <!-- Slider images End -->
+
+                        </div>
+                        <!-- Single Slider End  -->
+
+                        <!-- Single Slider Start  -->
+                        <div class="single-slider swiper-slide animation-style-01">
+
+                            <!-- Slider Content Start -->
+                            <div class="slider-content">
+                                <h2 class="title">Kumala <br> Savesta</h2>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas tempora repellat, labore sint itaque nihil dolor eaque accusantium, accusamus alias voluptatum tempore laborum.</p>
+                                <a href="product-details.html" class="btn btn-primary btn-hover-dark">purchase now</a>
+                            </div>
+                            <!-- Slider Content End -->
+
+                            <!-- Slider images Start -->
+                            <div class="slider-images">
+                                <img src="assets/images/slider/slider1.png" alt="Slider">
+                            </div>
+                            <!-- Slider images End -->
+
+                        </div>
+                        <!-- Single Slider End  -->
+
+                        <!-- Single Slider Start  -->
+                        <div class="single-slider swiper-slide animation-style-01">
+
+                            <!-- Slider Content Start -->
+                            <div class="slider-content">
+                                <h2 class="title">Oooomaga <br> acumalaka</h2>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas tempora repellat, labore sint itaque nihil dolor eaque accusantium, accusamus alias voluptatum tempore laborum.</p>
+                                <a href="product-details.html" class="btn btn-primary btn-hover-dark">purchase now</a>
+                            </div>
+                            <!-- Slider Content End -->
+
+                            <!-- Slider images Start -->
+                            <div class="slider-images">
+                                <img src="assets/images/slider/slider1.png" alt="Slider">
+                            </div>
+                            <!-- Slider images End -->
+
+                        </div>
+                        <!-- Single Slider End  -->
+
+                    </div>
+
+                    <!-- Add Pagination -->
+                    <div class="swiper-pagination"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Slider Section End -->
+
+    <!-- Benefit Section Start -->
+    <div class="section section-padding mt-n6">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6">
+
+                    <!-- Single Benefit Start -->
+                    <div class="single-benefit">
+                        <img src="assets/images/icon/logo-jadiUMKM.png" alt="Icon">
+                        <h3 class="title">Ooomaga</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore magnam perferendis unde asperiores fugit, aliquid delectus facilis laborum. Quam illo possimus eum fugit, inventore numquam aspernatur iure iste corrupti. Vitae accusamus fugit beatae perspiciatis ab enim aut, autem quos quasi blanditiis doloremque excepturi quisquam, ratione architecto id repellat, sed mollitia. Lorem ipsum dolor sit amet.</p>
+                    </div>
+                    <!-- Single Benefit End -->
+
+                </div>
+                <div class="col-lg-4 col-md-6">
+
+                    <!-- Single Benefit Start -->
+                    <div class="single-benefit">
+                        <img src="assets/images/icon/logo-jadiUMKM.png" alt="Icon">
+                        <h3 class="title">Ooomaga</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, porro. Mollitia numquam fuga alias, ab facilis ipsa animi natus omnis dolore eligendi vel iste delectus distinctio, ex minima accusamus exercitationem excepturi magni accusantium iure. Animi eaque consequatur ipsum quia. Error dignissimos perferendis delectus iure nemo laboriosam vitae voluptas ipsam. Natus.</p>
+                    </div>
+                    <!-- Single Benefit End -->
+
+                </div>
+                <div class="col-lg-4 col-md-6">
+
+                    <!-- Single Benefit Start -->
+                    <div class="single-benefit">
+                        <img src="assets/images/icon/logo-jadiUMKM.png" alt="Icon">
+                        <h3 class="title">Ooomaga</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum magni a, architecto laborum odit facere repudiandae fuga, repellendus itaque obcaecati officia laudantium, hic consectetur placeat fugiat. Ad, consequatur? Dolorem eos, ut adipisci dicta officia saepe ad repellendus quas est voluptas ratione libero autem dolorum veniam aliquid laborum voluptates debitis deserunt?</p>
+                    </div>
+                    <!-- Single Benefit End -->
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Benefit Section End -->
+
+    <!-- About Section Start -->
+    <div class="section section-padding-02" id="About">
+            <div class="container">
+    
+                <!-- History content End -->
+                <div class="history-content text-center">
+    
+                    <div class="section-title-03">
+                        <h6 class="sub-title">About Us</h6>
+                        <h2 class="title">jadiUMKM</h2>
+                    </div>
+    
+                    <p>Lorem ipsum dolor sit amet, consectet adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nullaotho pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natusxcl error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dictapteo sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem drt sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+    
+                </div>
+                <!-- History content End -->
+    
+                <!-- History Icon End -->
+                <div class="history-icon text-center">
+                    <!-- <img src="assets/images/icon/icon-5.jpg" alt="Icon"> -->
+                </div>
+                <!-- History Icon End -->
+    
+            </div>
+    </div>
+    <!-- About Section End -->
+    
+    <!-- Counter Section Start -->
+    <div class="section section-padding mt-n6">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-6">
+                        <!-- Single Counter Start -->
+                        <div class="single-counter">
+                            <span class="count"><span class="odometer" data-count-to="21"></span><sub>+</sub></span>
+                            <p> Years of Exprience</p>
+                        </div>
+                        <!-- Single Counter End -->
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <!-- Single Counter Start -->
+                        <div class="single-counter">
+                            <span class="count"><span class="odometer" data-count-to="30"></span><sub>K</sub></span>
+                            <p>Happy Customers</p>
+                        </div>
+                        <!-- Single Counter End -->
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <!-- Single Counter Start -->
+                        <div class="single-counter">
+                            <span class="count"><span class="odometer" data-count-to="15"></span><sub>+</sub></span>
+                            <p>Award Winner</p>
+                        </div>
+                        <!-- Single Counter End -->
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <!-- Single Counter Start -->
+                        <div class="single-counter">
+                            <span class="count"><span class="odometer" data-count-to="100"></span><sub>%</sub></span>
+                            <p>Products Guranteed</p>
+                        </div>
+                        <!-- Single Counter End -->
+                    </div>
+                </div>
+            </div>
+    </div>
+    <!-- Counter Section End -->
+
+    <!-- Contact Section Start -->
+    <div class="section section-padding" id="Contact">
+            <div class="container">
+    
+                <!-- Contact Wrapper Start -->
+                <div class="contact-wrapper">
+                    <div class="row gx-0">
+                        <div class="col-lg-4">
+                            <div class="contact-info">
+                                <h2 class="title">Contact Info</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit sed eiusmod</p>
+    
+                                <!-- Contact Info Items Start -->
+                                <div class="contact-info-items">
+    
+                                    <div class="single-contact-info">
+                                        <div class="info-icon">
+                                            <i class="pe-7s-call"></i>
+                                        </div>
+                                        <div class="info-content">
+                                            <p><a href="tel:+00125458222">+00 125 458 222</a></p>
+                                        </div>
+                                    </div>
+    
+                                    <div class="single-contact-info">
+                                        <div class="info-icon">
+                                            <i class="pe-7s-mail"></i>
+                                        </div>
+                                        <div class="info-content">
+                                            <p><a href="mailto:demo@yourmail.com">demo@yourmail.com</a></p>
+                                        </div>
+                                    </div>
+    
+                                    <div class="single-contact-info">
+                                        <div class="info-icon">
+                                            <i class="pe-7s-map-marker"></i>
+                                        </div>
+                                        <div class="info-content">
+                                            <p>Your Address Goese Here</p>
+                                        </div>
+                                    </div>
+    
+                                </div>
+                                <!-- Contact Info Items End -->
+    
+                                <!-- Contact Social Start -->
+                                <ul class="social">
+                                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-tumblr"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                                </ul>
+                                <!-- Contact Social End -->
+    
+                            </div>
+                        </div>
+                        <div class="col-lg-8">
+    
+                            <!-- Contact Form Start  -->
+                            <div class="contact-form">
+                                <form id="contact-form" action="" method="post">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="single-form">
+                                                <input type="text" name="name" placeholder="Name*">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="single-form">
+                                                <input type="email" name="email" placeholder="Email*">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="single-form">
+                                                <input type="text" name="subject" placeholder="Subject">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="single-form">
+                                                <input type="text" name="phone" placeholder="Phone No">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="single-form">
+                                                <textarea name="message" placeholder="Write your comments here"></textarea>
+                                            </div>
+                                        </div>
+                                        <p class="form-message"></p>
+                                        <div class="col-md-12">
+                                            <div class="single-form">
+                                                <button type="submit" class="btn btn-dark btn-hover-primary">Submit Review</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                            <!-- Contact Form End  -->
+    
+                        </div>
+                    </div>
+                </div>
+                <!-- Contact Wrapper End -->
+    
+            </div>
+    </div>
+    <!-- Contact Section End -->
+
+    @if (Auth::user())
             <div>
                 Kamu sudah masuk sebagai <span class="text-primary font-bold">{{ Auth::user()->role->name }}</span>
             </div>
@@ -13,5 +308,4 @@
                 Silahkan masuk atau daftar terlebih dahulu 
             </div>
         @endif
-    </div>
 </x-main-layout>
