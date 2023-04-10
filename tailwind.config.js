2de174ab232a165661952f8d7522464a0091375a
Fajrul Falah <fajrulf960@gmail.com>
2023-04-10 22:35:24 +0700

    landing page insyaallah fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,31 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
+        colors: {
+            unyu: "#fdf4ff",
+            oyen: "#ffe5d0",
+        },
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
         },
     },
 
-    plugins: [require('@tailwindcss/forms'), require("daisyui")],
+    plugins: [require("@tailwindcss/forms"), require("daisyui")],
+    variants:{
+        extend:{
+            display: ['group-focus'],
+            opacity: ['group-focus'],
+            inset: ['group-focus']
+        }
+    }
 };
