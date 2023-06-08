a0c825d44c3b06e482a20e9ce87cc0b3797e9d62
MaritsIkmalY <maritsikmaly@gmail.com>
2023-06-08 16:04:48 +0700

    update : refactor PortofolioController

--- app/Http/Requests/PortoRequest.php
+++ app/Http/Requests/PortoRequest.php
@@ -6,22 +6,15 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class PortoRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
-     */
     public function rules(): array
     {
         return [
+            'photo' => 'mimes:pdf,png,jpg,jpeg',
             'title' => ['required'],
             'description' => ['nullable'],
         ];
--- app/Models/ContentCreator.php
+++ app/Models/ContentCreator.php
@@ -6,8 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 
 class ContentCreator extends Model
 {
