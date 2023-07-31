0b6d262c2b6af092226ec6b13d5b5ac310ed948f
MaritsIkmalY <maritsikmaly@gmail.com>
2023-07-31 07:15:21 +0700

    npm run build

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -34,7 +34,6 @@
 			</li>
 			<li><a class="text-sm text-gray-400 hover:text-gray-500" href="{{route('katalog')}}">Katalog UMKM</a></li>
 			<li class="text-gray-300">
-				<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" class="w-4 h-4 current-fill" viewBox="0 0 24 24">
 					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
 				</svg>
 			</li>
