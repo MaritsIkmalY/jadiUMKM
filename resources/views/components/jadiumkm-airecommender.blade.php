3b0a15d78915ab8c2935eef50c598b73386e875d
Nicholaus Adhyatma <nicholausadhyatma@gmail.com>
2023-08-01 16:39:10 +0700

    clear

--- resources/views/components/jadiumkm-airecommender.blade.php
+++ resources/views/components/jadiumkm-airecommender.blade.php
@@ -1,5 +1,5 @@
 <div
-    class="max-w-md p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-auto">
+    class="max-w-2xl p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-auto">
     <form {{ $attributes->merge(['method' => 'post']) }} action="{{ route('airecommender') }}">
         @csrf
         <div class="mb-4">
