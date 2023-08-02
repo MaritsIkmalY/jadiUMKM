687c836cdb3aae5294fce07c95d8e4d7f2945aef
Nicholaus Adhyatma <nicholausadhyatma@gmail.com>
2023-08-02 20:15:07 +0700

    freelancer ui refactor

--- resources/views/components/jadiumkm-btn.blade.php
+++ resources/views/components/jadiumkm-btn.blade.php
@@ -3,7 +3,7 @@
 @php
     $variantClass = [
         'primary' => 'text-white font-medium bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-600 focus:outline-none dark:focus:ring-blue-800',
-        'secondary' => 'py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700',
+        'secondary' => 'py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-blue-600 focus:outline-none bg-white rounded-lg border border-blue-600 hover:bg-blue-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-700 dark:bg-blue-800 dark:text-blue-400 dark:border-blue-600 dark:hover:text-white dark:hover:bg-blue-700',
     ];
 @endphp
 
--- resources/views/components/jadiumkm-header.blade.php
+++ resources/views/components/jadiumkm-header.blade.php
@@ -1,12 +1,15 @@
-@props(['title' => 'title', 'back' => ''])
+@props(['title' => 'title', 'back' => 'home', 'isBack' => true])
 
 <div class="max-w-7xl mx-auto p-4 min-h-screen">
-    <x-jadiumkm-btn variant="secondary" class="flex gap-2 items-center" href="{{ route($back) }}"><svg
-            class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-            fill="none" viewBox="0 0 14 10">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M13 5H1m0 0 4 4M1 5l4-4" />
-        </svg>Kembali</x-jadiumkm-btn>
+
+    @if ($isBack)
+        <x-jadiumkm-btn variant="secondary" class="flex gap-2 items-center" href="{{ route($back) }}"><svg
+                class="w-4 h-4 text-blue-600 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M13 5H1m0 0 4 4M1 5l4-4" />
+            </svg>Kembali</x-jadiumkm-btn>
+    @endif
     <h1 class="mt-4 font-bold text-2xl text-center">{{ $title }}</h1>
 
     <div class="flex items-center justify-center w-full flex-wrap gap-4 mt-5">
