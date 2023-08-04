6388dacf79db78d88b99595d1be069b17a1fa5c3
Nicholaus Adhyatma <nicholausadhyatma@gmail.com>
2023-08-04 22:45:50 +0700

    live-wire-bug

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,7 +11,7 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
-    <livewire:styles>
+    @livewireStyles
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>JadiUMKM</title>
     
