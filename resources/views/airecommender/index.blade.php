66150ad9cfb3fab6b33633c636a079c87782a648
Nicholaus Adhyatma <nicholausadhyatma@gmail.com>
2023-08-04 22:57:05 +0700

    airecommender-livewire

--- resources/views/airecommender/index.blade.php
+++ resources/views/airecommender/index.blade.php
@@ -2,7 +2,7 @@
     <div class="max-w-7xl m-auto p-4 min-h-screen">
         <div class="text-2xl font-bold mb-4 text-center">Ai Recommender</div>
 
-        <livewire:airecommender />
+        @livewire('airecommender')
 
     </div>
 </x-main-layout>
